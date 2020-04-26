991ee5b932a7a9317cf99983b619364176c0d520
KostyaJRZ <jrzroyale@gmail.com>
2020-04-27 01:14:23 +0300

    build: Bring-up Incremental OTA

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -2061,31 +2061,36 @@ else if get_stage("%(bcb_dev)s") != "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("org.pixelplusui.device")
 
   prev_build_id = source_info.GetBuildProp("ro.build.id")
   build_id = target_info.GetBuildProp("ro.build.id")
 
-  prev_build_date = source_info.GetBuildProp("org.pixelexperience.build_date")
-  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  prev_build_date = source_info.GetBuildProp("org.pixelplusui.build_date")
+  build_date = target_info.GetBuildProp("org.pixelplusui.build_date")
 
   prev_security_patch = source_info.GetBuildProp("ro.build.version.security_patch")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
 
   script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
+  script.Print("          ______  ______  __    __  ___      ");
+  script.Print("         / __  / / __  / / /   / / /  /      ");
+  script.Print("        / /_/ / / /_/ / / /   / / /  /       ");
+  script.Print("       /  ___/ /  ___/ / /   / / /  /        ");
+  script.Print("      /  /    /  /    / /___/ / /  /         ");
+  script.Print("     /__/    /__/     \______/ /__/          ");
   script.Print("----------------------------------------------");
-  script.Print(" Android version: %s"%(android_version));
+  script.Print(" ROM by: @kostyajrz");
+  script.Print(" Android Version: %s"%(android_version));
   if prev_build_id != build_id:
-    script.Print(" Build id: %s -> %s"%(prev_build_id, build_id));
+    script.Print(" Build ID: %s -> %s"%(prev_build_id, build_id));
   else:
-    script.Print(" Build id: %s"%(build_id));
-  script.Print(" Build date: %s -> %s"%(prev_build_date, build_date));
+    script.Print(" Build ID: %s"%(build_id));
+  script.Print(" Build Time: %s -> %s"%(prev_build_date, build_date));
   if prev_security_patch != security_patch:
-    script.Print(" Security patch: %s -> %s"%(prev_security_patch, security_patch));
+    script.Print(" Security Patch: %s -> %s"%(prev_security_patch, security_patch));
   else:
-    script.Print(" Security patch: %s"%(security_patch));
+    script.Print(" Security Patch: %s"%(security_patch));
   script.Print(" Device: %s"%(device));
   script.Print("----------------------------------------------");
 
@@ -2590,31 +2595,36 @@ def WriteIncrementalOTAPackage(target_zip, source_zip, output_file):
   device_specific.IncrementalOTA_Assertions()
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("org.pixelplusui.device")
 
   prev_build_id = source_info.GetBuildProp("ro.build.id")
   build_id = target_info.GetBuildProp("ro.build.id")
 
-  prev_build_date = source_info.GetBuildProp("org.pixelexperience.build_date")
-  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  prev_build_date = source_info.GetBuildProp("org.pixelplusui.build_date")
+  build_date = target_info.GetBuildProp("org.pixelplusui.build_date")
 
   prev_security_patch = source_info.GetBuildProp("ro.build.version.security_patch")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
 
   script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
+  script.Print("          ______  ______  __    __  ___      ");
+  script.Print("         / __  / / __  / / /   / / /  /      ");
+  script.Print("        / /_/ / / /_/ / / /   / / /  /       ");
+  script.Print("       /  ___/ /  ___/ / /   / / /  /        ");
+  script.Print("      /  /    /  /    / /___/ / /  /         ");
+  script.Print("     /__/    /__/     \______/ /__/          ");
   script.Print("----------------------------------------------");
-  script.Print(" Android version: %s"%(android_version));
+  script.Print(" ROM by: @kostyajrz");
+  script.Print(" Android Version: %s"%(android_version));
   if prev_build_id != build_id:
-    script.Print(" Build id: %s -> %s"%(prev_build_id, build_id));
+    script.Print(" Build ID: %s -> %s"%(prev_build_id, build_id));
   else:
-    script.Print(" Build id: %s"%(build_id));
-  script.Print(" Build date: %s -> %s"%(prev_build_date, build_date));
+    script.Print(" Build ID: %s"%(build_id));
+  script.Print(" Build Time: %s -> %s"%(prev_build_date, build_date));
   if prev_security_patch != security_patch:
-    script.Print(" Security patch: %s -> %s"%(prev_security_patch, security_patch));
+    script.Print(" Security Patch: %s -> %s"%(prev_security_patch, security_patch));
   else:
-    script.Print(" Security patch: %s"%(security_patch));
+    script.Print(" Security Patch: %s"%(security_patch));
   script.Print(" Device: %s"%(device));
   script.Print("----------------------------------------------");
 
