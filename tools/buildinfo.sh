57452460211b65b88265fd18589bf240516e203d
ugly-kid-af <dwarmachine24@gmail.com>
2021-12-03 12:48:05 +0530

    build: Initial PixelPlusUI Commit
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -57,6 +57,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "org.pixelexperience.device=$CUSTOM_DEVICE"
+echo "org.pixelplusui.device=$CUSTOM_DEVICE"
 
 echo "# end build properties"
